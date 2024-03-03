e5de858284740dfa6ab536dd07cc859c5b2d65c6
Scalaptia <fernando@haroware.com>
2024-03-03 12:12:14 -0800

    Image optimization and minor tweeks

--- src/modules/home.ts
+++ src/modules/home.ts
@@ -1,51 +1,56 @@
-import '../style/home.css';
-import Logo from '../assets/logo.png';
-import pageLoad from './pageLoad';
-import Menu from './menu';
-
+import "../style/home.css";
+import Logo from "../assets/logo.png";
+import pageLoad from "./pageLoad";
+import Menu from "./menu";
 
 export default function Hero() {
-    const container = document.createElement('div');
-    container.classList.add('tab-container', 'home-container')
+    const container = document.createElement("div");
+    container.classList.add("tab-container", "home-container");
 
-    const heroLeft = document.createElement('div');
-    heroLeft.classList.add('hero-left');
+    const heroLeft = document.createElement("div");
+    heroLeft.classList.add("hero-left");
 
-        const logoImage = new Image();
-        logoImage.src = Logo;
-        logoImage.classList.add('logo');
-        heroLeft.appendChild(logoImage);
+    const logoImage = new Image();
+    logoImage.src = Logo;
+    logoImage.classList.add("logo");
+    heroLeft.appendChild(logoImage);
 
-    const heroRight = document.createElement('div');
-    heroRight.classList.add('hero-right');
+    const heroRight = document.createElement("div");
+    heroRight.classList.add("hero-right");
 
-        const heroTitle = document.createElement('h1');
-        heroTitle.classList.add('hero-title');
-        heroTitle.innerText = 'Welcome to Meow Café!'
-        heroRight.appendChild(heroTitle)
+    const heroTitle = document.createElement("h1");
+    heroTitle.classList.add("hero-title");
+    heroTitle.innerText = "Welcome to Meow Café!";
+    heroRight.appendChild(heroTitle);
 
-        const heroText = document.createElement('h2');
-        heroText.classList.add('hero-text');
-        heroText.innerText = 'Escape to a world of purrs and relaxation at our cat cafe.';
-        heroRight.appendChild(heroText);
-    
-        const heroCTA = document.createElement('div');
-        heroCTA.classList.add('hero-cta');
+    const heroText = document.createElement("h2");
+    heroText.classList.add("hero-text");
+    heroText.innerText =
+        "Escape to a world of purrs and relaxation at our cat cafe.";
+    heroRight.appendChild(heroText);
 
-            const menuBtn = document.createElement('button');
-            menuBtn.classList.add('menu-btn');
-            menuBtn.innerText = 'Explore our menu'
-            menuBtn.addEventListener('click', () => {pageLoad(Menu())})
-            heroCTA.appendChild(menuBtn);
+    const heroCTA = document.createElement("div");
+    heroCTA.classList.add("hero-cta");
 
-            const igBtn = document.createElement('button');
-            igBtn.classList.add('ig-btn');
-            heroCTA.appendChild(igBtn);
+    const menuBtn = document.createElement("button");
+    menuBtn.classList.add("menu-btn");
+    menuBtn.innerText = "Explore our menu";
+    menuBtn.addEventListener("click", () => {
+        pageLoad(Menu());
+    });
+    heroCTA.appendChild(menuBtn);
 
-            heroRight.appendChild(heroCTA)
+    const igBtn = document.createElement("button");
+    igBtn.classList.add("ig-btn");
+    igBtn.onclick = function () {
+        window.open("https://www.instagram.com/harogato_/", "_blank");
+    };
+    heroCTA.appendChild(igBtn);
 
-        container.appendChild(heroLeft);
-        container.appendChild(heroRight);
+    heroRight.appendChild(heroCTA);
 
-    return container
+    container.appendChild(heroLeft);
+    container.appendChild(heroRight);
+
+    return container;
 }
--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -1,79 +1,85 @@
-import '../style/menu.css';
+import "../style/menu.css";
 
-function getImgUrl(fileName: string){
-    const imgUrl = new URL(`../assets/menu-items/${fileName}.png`, import.meta.url).href
-    return imgUrl
+function getImgUrl(fileName: string) {
+    const imgUrl = new URL(
+        `../assets/menu-items/${fileName}.png`,
+        import.meta.url
+    ).href;
+    return imgUrl;
 }
 
-const createItem = (name:string, price:number, imageUrl: string) => {
-    const itemContainer = document.createElement('div');
-    itemContainer.classList.add('item-container');
+const createItem = (name: string, price: number, imageUrl: string) => {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
 
-        const itemImage = document.createElement('img');
-        itemImage.classList.add('item-image');
-        itemImage.src = imageUrl
-        itemContainer.appendChild(itemImage)
+    const itemImage = document.createElement("img");
+    itemImage.classList.add("item-image");
+    itemImage.src = imageUrl;
+    itemContainer.appendChild(itemImage);
 
-        const itemName = document.createElement('h3');
-        itemName.classList.add('item-name');
-        itemName.innerText = `${name}`;
-        itemContainer.appendChild(itemName);
+    const itemName = document.createElement("h3");
+    itemName.classList.add("item-name");
+    itemName.innerText = `${name}`;
+    itemContainer.appendChild(itemName);
 
-        const itemPrice = document.createElement('p');
-        itemPrice.classList.add('item-price');
-        itemPrice.innerText = `$${price.toFixed(2)}`;
-        itemContainer.appendChild(itemPrice);
+    const itemPrice = document.createElement("p");
+    itemPrice.classList.add("item-price");
+    itemPrice.innerText = `$${price.toFixed(2)}`;
+    itemContainer.appendChild(itemPrice);
 
     return itemContainer;
-}
+};
 
 export default function Menu() {
-    const container = document.createElement('div');
-    container.classList.add('tab-container', 'menu-container');
+    const container = document.createElement("div");
+    container.classList.add("tab-container", "menu-container");
 
-    const menuTitle = document.createElement('h1');
-    menuTitle.classList.add('menu-title');
-    menuTitle.innerText = 'Menu'
-    container.appendChild(menuTitle);
-    
-    const warmMenuTitle = document.createElement('h3');
-    warmMenuTitle.innerText = 'Warm Kitty'
-    container.appendChild(warmMenuTitle)
+    const warmMenuTitle = document.createElement("h3");
+    warmMenuTitle.innerText = "Warm Kitty";
+    container.appendChild(warmMenuTitle);
 
-    const warmMenu = document.createElement('div');
-    warmMenu.classList.add('warm-menu', 'menu');
-        warmMenu.appendChild(createItem('Espresso', 3.5, getImgUrl('espresso')));
-        warmMenu.appendChild(createItem('Cappuccino', 4, getImgUrl('capuccino')));
-        warmMenu.appendChild(createItem('Latte', 4, getImgUrl('latte')));
-        warmMenu.appendChild(createItem('Americano', 3.75, getImgUrl('americano')));
-        warmMenu.appendChild(createItem('Hot Tea', 2.5, getImgUrl('hot_tea')));
-        warmMenu.appendChild(createItem('Chai Latte', 4, getImgUrl('chai_latte')));
-        warmMenu.appendChild(createItem('Hot Chocolate', 4, getImgUrl('hot_chocolate')));
+    const warmMenu = document.createElement("div");
+    warmMenu.classList.add("warm-menu", "menu");
+    warmMenu.appendChild(createItem("Espresso", 3.5, getImgUrl("espresso")));
+    warmMenu.appendChild(createItem("Cappuccino", 4, getImgUrl("capuccino")));
+    warmMenu.appendChild(createItem("Latte", 4, getImgUrl("latte")));
+    warmMenu.appendChild(createItem("Americano", 3.75, getImgUrl("americano")));
+    warmMenu.appendChild(createItem("Hot Tea", 2.5, getImgUrl("hot_tea")));
+    warmMenu.appendChild(createItem("Chai Latte", 4, getImgUrl("chai_latte")));
+    warmMenu.appendChild(
+        createItem("Hot Chocolate", 4, getImgUrl("hot_chocolate"))
+    );
     container.appendChild(warmMenu);
 
-    const coldMenuTitle = document.createElement('h3');
-    coldMenuTitle.innerText = 'Cool Cat'
-    container.appendChild(coldMenuTitle)
+    const coldMenuTitle = document.createElement("h3");
+    coldMenuTitle.innerText = "Cool Cat";
+    container.appendChild(coldMenuTitle);
 
-    const coldMenu = document.createElement('div');
-    coldMenu.classList.add('cold-menu', 'menu');
-        coldMenu.appendChild(createItem('Iced Tea', 3, getImgUrl('iced_tea')));
-        coldMenu.appendChild(createItem('Iced Mocha', 5, getImgUrl('iced_mocha')));
-        coldMenu.appendChild(createItem('Iced Chai', 4.5, getImgUrl('iced_chai')));
-        coldMenu.appendChild(createItem('Mocha Frappe', 6.25, getImgUrl('mocha_frappe')));
-        coldMenu.appendChild(createItem('Caramel Frappe', 6.25, getImgUrl('caramel_frappe')));
+    const coldMenu = document.createElement("div");
+    coldMenu.classList.add("cold-menu", "menu");
+    coldMenu.appendChild(createItem("Iced Tea", 3, getImgUrl("iced_tea")));
+    coldMenu.appendChild(createItem("Iced Mocha", 5, getImgUrl("iced_mocha")));
+    coldMenu.appendChild(createItem("Iced Chai", 4.5, getImgUrl("iced_chai")));
+    coldMenu.appendChild(
+        createItem("Mocha Frappe", 6.25, getImgUrl("mocha_frappe"))
+    );
+    coldMenu.appendChild(
+        createItem("Caramel Frappe", 6.25, getImgUrl("caramel_frappe"))
+    );
     container.appendChild(coldMenu);
 
-    const foodMenuTitle = document.createElement('h3');
-    foodMenuTitle.innerText = 'Food'
-    container.appendChild(foodMenuTitle)
+    const foodMenuTitle = document.createElement("h3");
+    foodMenuTitle.innerText = "Food";
+    container.appendChild(foodMenuTitle);
 
-    const foodMenu = document.createElement('div');
-    foodMenu.classList.add('food-menu', 'menu');
-        foodMenu.appendChild(createItem('Pastries', 3.5, getImgUrl('pastries')));
-        foodMenu.appendChild(createItem('Sandwiches', 6, getImgUrl('sandwiches')));
-        foodMenu.appendChild(createItem('Salads', 6, getImgUrl('salads')));
-        foodMenu.appendChild(createItem('Snack Trays', 6, getImgUrl('snack_trays')));
+    const foodMenu = document.createElement("div");
+    foodMenu.classList.add("food-menu", "menu");
+    foodMenu.appendChild(createItem("Pastries", 3.5, getImgUrl("pastries")));
+    foodMenu.appendChild(createItem("Sandwiches", 6, getImgUrl("sandwiches")));
+    foodMenu.appendChild(createItem("Salads", 6, getImgUrl("salads")));
+    foodMenu.appendChild(
+        createItem("Snack Trays", 6, getImgUrl("snack_trays"))
+    );
     container.appendChild(foodMenu);
 
     return container;
